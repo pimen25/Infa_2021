735a5f1276d40864effce0f26dd6fa2ed48adc6b
Nikita Nikita <zorro.pimenov@gmail.com>
2021-03-27 09:08:33 +0300

    init

---
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+
+namespace MVC.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
